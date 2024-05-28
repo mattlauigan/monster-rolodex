fa578a7043dc79b853ca9af9a4e9ed721946216e
Matt Lauigan <mat.lauigan@corewaretech.com.ph>
2024-05-29 06:41:53 +0800

    update app.js file

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,19 +1,27 @@
 import { useState, useEffect } from "react";
-
+import { useQuery } from "react-query";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
+
+const fetchUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
 
 const App = () => {
   const [monsters, setMonsters] = useState([]);
   const [searchField, setSearchField] = useState("");
   const [filteredMonster, setFileteredMonster] = useState(monsters);
+  const { data, status } = useQuery("users", fetchUsers);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
-  }, []);
+  console.log(data);
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => setMonsters(users));
+  // }, []);
 
   useEffect(() => {
     const newfilteredMonster = monsters.filter((monster) => {
